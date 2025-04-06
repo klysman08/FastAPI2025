c7f6a7444c34eb914ff63b5e7d3615c8eb645d84
klysman08 <klysman.rk@gmail.com>
2025-04-06 22:09:24 +0100

    Add user management endpoints with CRUD operations and update schemas

--- src/fast_zero/app.py
+++ src/fast_zero/app.py
@@ -1,12 +1,54 @@
 from http import HTTPStatus
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
-from fast_zero.schemas import Message
+from fast_zero.schemas import Message, UserDB, UserList, UserPublic, UserSchema
 
 app = FastAPI()
+
+
+database = []
 
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return Message(message='Olá Mundo!')
+
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
+
+    database.append(user_with_id)
+
+    return user_with_id
+
+
+@app.get('/users/', status_code=HTTPStatus.OK, response_model=UserList)
+def get_users():
+    return {'users': database}
+
+
+@app.put('/users/{user_id}', response_model=UserPublic)
+def update_user(user_id: int, user: UserSchema):
+    if user_id > len(database):
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, message='User not found'
+        )
+
+    user_with_id = UserDB(**user.model_dump(), id=user_id)
+    database[user_id - 1] = user_with_id
+
+    return user_with_id
+
+
+@app.delete('/users/{user_id}', response_model=Message)
+def delete_user(user_id: int):
+    if user_id > len(database):
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, message='User not found'
+        )
+
+    del database[user_id - 1]
+
+    return {'message': 'User deleted'}
